3aa41eb1243d3f516a495cc515ecdeb137c32a84
Arek Rado <ArekRado@users.noreply.github.com>
2024-02-20 01:08:18 +0700

    background and player movement

--- characters/player.gd
+++ characters/player.gd
@@ -1,73 +1,71 @@
 extends Area2D
 
 @export var speed: float = 200.0
-@export var jump_velocity: float = -150.0
-@export var double_jump_velocity: float = -100.0
+@export var jump_speed: float = -3.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var shadow_sprite: Sprite2D = $Shadow
+@onready var shadow_raycast: RayCast2D = $ShadowRaycast2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var player_gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
-var shadow_position_y: float = 0.0
-var has_double_jump: bool = false
 var animation_locked: bool = false
 var direction: Vector2 = Vector2.ZERO
+var jump_velocity: float = 0.0
 var velocity: Vector2 = Vector2.ZERO
 var was_in_air: bool = false
-var is_on_floor: bool = false
+var is_on_floor: bool = true
+var shadow_shift_y: float = 0.0
 
+func _process(delta: float) -> void:
+	if shadow_raycast.is_colliding:
+		shadow_sprite.position = to_local(shadow_raycast.get_collision_point())
+		shadow_sprite.position.y += shadow_shift_y
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if is_on_floor == false:
-		player_gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-		velocity.y += player_gravity * delta
-		was_in_air= true
-	else: 
-		has_double_jump = false
-		
-		if was_in_air == true:
-			land()
-			
+		jump_velocity += (player_gravity * delta) / Engine.physics_ticks_per_second
+		was_in_air = true
+	elif was_in_air == true:
+		land()
 		was_in_air = false
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump"):
-		if is_on_floor == true:
-			jump()
-		elif not has_double_jump:
-			velocity.y = double_jump_velocity
-			has_double_jump = true
+	if Input.is_action_just_pressed("jump") && is_on_floor == true:
+		jump_start()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	direction = Input.get_vector("left", "right", "up", "down")
-	if direction.x != 0:
-		velocity.x = direction.x * speed
+	if direction.length() != 0:
+		velocity = direction * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.y = move_toward(velocity.y, 0, speed)
 		
-	if position.y <= shadow_position_y:
-		velocity.y = 0
+	if is_on_floor == false && global_position.y >  shadow_sprite.global_position.y:
+		global_position.y =  shadow_sprite.global_position.y
+		jump_velocity = 0.0
 		is_on_floor = true
 		
+	jump()
 	move()
 	
 	#update_animation()
 	update_facing_direction()
 	
-func _input(event: InputEvent) -> void:
-	if animated_sprite.animation == 'run':
-		shadow_sprite.position = position
-	elif animated_sprite.animation == 'jump_start' || animated_sprite.animation == 'jump_end':
-		shadow_sprite.position.x = position.x
+#func _input(event: InputEvent) -> void:
+	#if animated_sprite.animation == 'run':
+		#shadow_sprite.position = position
+	#elif animated_sprite.animation == 'jump_start' || animated_sprite.animation == 'jump_end':
+		#shadow_sprite.position.x = position.x
 		
 func move() -> void:
 	position += velocity / Engine.physics_ticks_per_second
+	shadow_shift_y += velocity.y / Engine.physics_ticks_per_second
+	shadow_sprite.position.x = position.x
 	
-func _on_body_entered(body:Node) -> void:
-	print("_on_body_entered", body.name)
+#func _on_body_entered(body:Node) -> void:
+	#if body.name == 'Ground':
+		#is_on_floor = true
+		#velocity = Vector2.ZERO
 		
 func update_animation() -> void:
 	if not animation_locked:
@@ -82,12 +80,15 @@ func update_facing_direction() -> void:
 	elif direction.x < 0:
 		animated_sprite.flip_h = true
 		
-func jump() -> void:
+func jump_start() -> void:
 	is_on_floor = false
-	velocity.y = jump_velocity
-	shadow_position_y = position.y
+	jump_velocity = jump_speed
+	shadow_sprite.position.y = position.y
 	animated_sprite.play('jump_start')
 	animation_locked = true
+	
+func jump() -> void:
+	position.y += jump_velocity
 	
 func land() -> void:
 	animated_sprite.play('jump_end')
