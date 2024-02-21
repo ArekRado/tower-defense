d43c113ff6aa2ecc71779819266c9e79548f71ff
Arek Rado <ArekRado@users.noreply.github.com>
2024-02-22 01:26:26 +0700

    Basic platform jumping

--- characters/player.gd
+++ characters/player.gd
@@ -15,12 +15,14 @@ var jump_velocity: float = 0.0
 var velocity: Vector2 = Vector2.ZERO
 var was_in_air: bool = false
 var is_on_floor: bool = true
-var shadow_shift_y: float = 0.0
+var shadow_shift_y: float = -5.0
 
 func _process(delta: float) -> void:
-	if shadow_raycast.is_colliding:
-		shadow_sprite.position = to_local(shadow_raycast.get_collision_point())
-		shadow_sprite.position.y += shadow_shift_y
+	if shadow_raycast.is_colliding():
+		var collision_position: Vector2 = to_local(shadow_raycast.get_collision_point())
+
+		shadow_sprite.position.x = collision_position.x
+		shadow_sprite.position.y = collision_position.y + shadow_shift_y
 
 func _physics_process(delta: float) -> void:
 	if is_on_floor == false:
@@ -35,16 +37,11 @@ func _physics_process(delta: float) -> void:
 
 	direction = Input.get_vector("left", "right", "up", "down")
 	if direction.length() != 0:
-		velocity = direction * speed
+		velocity = (direction * speed * delta) * Engine.physics_ticks_per_second
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.y = move_toward(velocity.y, 0, speed)
-		
-	if is_on_floor == false && global_position.y >  shadow_sprite.global_position.y:
-		global_position.y =  shadow_sprite.global_position.y
-		jump_velocity = 0.0
-		is_on_floor = true
-		
+	
 	jump()
 	move()
 	
@@ -58,9 +55,17 @@ func _physics_process(delta: float) -> void:
 		#shadow_sprite.position.x = position.x
 		
 func move() -> void:
-	position += velocity / Engine.physics_ticks_per_second
 	shadow_shift_y += velocity.y / Engine.physics_ticks_per_second
-	shadow_sprite.position.x = position.x
+	position += velocity / Engine.physics_ticks_per_second
+	#shadow_sprite.position.x = position.x
+	#shadow_sprite.position.x += velocity.y / Engine.physics_ticks_per_second
+	
+	if jump_velocity == 0 && shadow_raycast.is_colliding() == false:
+		shadow_sprite.hide()
+		#jump_start()
+		is_on_floor = false
+	elif shadow_sprite.hidden:
+		shadow_sprite.show()
 	
 #func _on_body_entered(body:Node) -> void:
 	#if body.name == 'Ground':
@@ -80,18 +85,27 @@ func update_facing_direction() -> void:
 	elif direction.x < 0:
 		animated_sprite.flip_h = true
 		
+func get_sprite_size() -> Vector2:
+	return animated_sprite.sprite_frames.get_frame_texture("idle", 0).get_size() * animated_sprite.get_scale()
+		
 func jump_start() -> void:
 	is_on_floor = false
 	jump_velocity = jump_speed
-	shadow_sprite.position.y = position.y
+	
 	animated_sprite.play('jump_start')
 	animation_locked = true
 	
 func jump() -> void:
+	var will_touch_shadow: bool = global_position.y + jump_velocity > shadow_sprite.global_position.y
+	if is_on_floor == false && shadow_raycast.is_colliding() && will_touch_shadow:
+		global_position.y = shadow_sprite.global_position.y
+		jump_velocity = 0.0
+		is_on_floor = true
+		
 	position.y += jump_velocity
 	
 func land() -> void:
-	animated_sprite.play('jump_end')
+	animated_sprite.play('idle')
 	animation_locked = true
 
 func _on_animated_sprite_2d_animation_finished() -> void:
