9e84eea7766a0a3fb22debc4edb1c7ca01797100
Arek Rado <ArekRado@users.noreply.github.com>
2024-04-22 01:19:10 +0700

    baracks

--- structures/baracks/baracks.gd
+++ structures/baracks/baracks.gd
@@ -1,18 +1,26 @@
-extends Node
+extends Node2D
 class_name Baracks
 
-@export var unit_creation_max_time: float = 1.0
-var unit_creation_time: float = 0.0
+@export var character_amount_max: int = 3
+@export var character_creation_max_time: float = 1.0
+var character_creation_time: float = 0.0
 
+@onready var structure: Structure = $".."
 @onready var recruit: PackedScene = preload("res://characters/recruit/recruit.tscn")
 var recruitInstance: Character
 
 func _ready() -> void:
-	unit_creation_time = unit_creation_max_time
+	character_creation_time = character_creation_max_time
 
 func _process(delta: float) -> void:
-	unit_creation_time -= delta 
-	if unit_creation_time < 0:
-		unit_creation_time = unit_creation_max_time
+	if structure.assigned_characters.size() >= character_amount_max:
+		return
+		
+	character_creation_time -= delta 
+	if character_creation_time < 0:
+		character_creation_time = character_creation_max_time
 		recruitInstance = recruit.instantiate()
 		get_tree().get_root().add_child(recruitInstance)
+		recruitInstance.global_position = global_position
+		
+		structure.assign_character(recruitInstance)
--- structures/structure.gd
+++ structures/structure.gd
@@ -1,6 +1,8 @@
 extends Node
 class_name Structure
 
+var assigned_characters: Array[Character] = []
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,3 +11,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+func assign_character(character:Character) -> void:
+	assigned_characters.push_back(character)
+
+func unassign_character(character:Character) -> void:
+	assigned_characters = assigned_characters.filter(func (c:Character) -> bool: 
+		return character.get_instance_id() != c.get_instance_id()
+	)
